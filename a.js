dfa56fa6de5111af2798330bd6fd9b6c3f42b81f
Andrew <64666648+Diaresta@users.noreply.github.com>
2022-12-02 22:39:28 -0500

    AoC '22 Day 2 Answer: A

--- a.js
+++ a.js
@@ -5,7 +5,34 @@ const input = fs.readFileSync('input.txt', 'utf-8').split('\n');
 const rpsCheck = (input) => {
   let total = 0;
 
-  console.log(input[0]);
+  for (let i = 0; i < input.length; i++) {
+    if (input[i][0] === 'A') {
+      if (input[i][2] === 'X') {
+        total += 4;
+      } else if (input[i][2] === 'Y') {
+        total += 8;
+      } else if (input[i][2] === 'Z') {
+        total += 3;
+      }
+    } else if (input[i][0] === 'B') {
+      if (input[i][2] === 'X') {
+        total += 1;
+      } else if (input[i][2] === 'Y') {
+        total += 5;
+      } else if (input[i][2] === 'Z') {
+        total += 9;
+      }
+    } else if (input[i][0] === 'C') {
+      if (input[i][2] === 'X') {
+        total += 7;
+      } else if (input[i][2] === 'Y') {
+        total += 2;
+      } else if (input[i][2] === 'Z') {
+        total += 6;
+      }
+    }
+  }
+  console.log(total);
 };
 
 rpsCheck(input);
