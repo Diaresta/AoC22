df972fc799985796baa9e4cf09d6644407a76313
Andrew <64666648+Diaresta@users.noreply.github.com>
2022-12-02 21:52:11 -0500

    Starting Day 2

---
+++ a.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('input.txt', 'utf-8').split('\n');
+
+const rpsCheck = (input) => {
+  let total = 0;
+
+  console.log(input[0]);
+};
+
+rpsCheck(input);
