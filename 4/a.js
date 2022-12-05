4cf79c1251c840cd1912de46830a9583026334e5
Andrew <64666648+Diaresta@users.noreply.github.com>
2022-12-05 00:37:01 -0500

    AoC '22 Day 4 Answer: A

--- 4/a.js
+++ 4/a.js
@@ -1,7 +1,26 @@
 const fs = require('fs');
 
-const input = fs.readFileSync('4/input.txt', 'utf-8').split('\n');
+const input = fs.readFileSync('4/input.txt', 'utf-8');
+const inputFix = input.split('\n').map((row) => row.split(','));
 
-const cleanup = (input) => {};
+const cleanup = (input) => {
+  let output = 0;
 
-cleanup(input);
+  for (let i = 0; i < input.length; i++) {
+    let [firstStart, firstEnd] = input[i][0]
+      .split('-')
+      .map((num) => parseInt(num));
+    let [secondStart, secondEnd] = input[i][1]
+      .split('-')
+      .map((num) => parseInt(num));
+
+    if (firstStart >= secondStart && firstEnd <= secondEnd) {
+      output++;
+    } else if (firstStart <= secondStart && firstEnd >= secondEnd) {
+      output++;
+    }
+  }
+  console.log(output);
+};
+
+cleanup(inputFix);
