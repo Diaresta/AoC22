cc1eabae305379d71af135c661fb3a1c9a4355cd
Andrew <64666648+Diaresta@users.noreply.github.com>
2022-12-06 23:37:33 -0500

    AoC '22 Day 6 Answer: A

--- 6/a.js
+++ 6/a.js
@@ -3,7 +3,17 @@ const fs = require('fs');
 const input = fs.readFileSync('6/input.txt', 'utf-8');
 
 const packetMarker = (input) => {
-  console.log();
+  for (let i = 0; i < input.length; i++) {
+    let letterGroup = input.slice(i - 4, i);
+
+    const letterSet = new Set(letterGroup);
+
+    if (letterSet.size === 4) {
+      output = i;
+      console.log(output);
+      break;
+    }
+  }
 };
 
 packetMarker(input);
