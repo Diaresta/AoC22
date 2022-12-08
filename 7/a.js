022828c23ab434fdbd9a9001b2b15fc4f84afd45
Andrew <64666648+Diaresta@users.noreply.github.com>
2022-12-07 23:03:41 -0500

    Starting Day 7

--- 7/a.js
+++ 7/a.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const input = fs.readFileSync('2/input.txt', 'utf-8').split('\n');
+const input = fs.readFileSync('7/input.txt', 'utf-8').split('\n');
 
 const deviceCheck = (input) => {};
 
